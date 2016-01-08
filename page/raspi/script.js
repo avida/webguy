dec3d729aa781511d8cd05803a30fe5b90a1edde
avida.d3 <avida.d3@gmail.com>
2016-01-08 18:49:54 +0000

    remove common substring in filenames

--- page/raspi/script.js
+++ page/raspi/script.js
@@ -1,4 +1,192 @@
 
+//-----------------File browser station-------------------------//
+
+function find_longest_substr(s1, s2) {
+  // js implementation of algorythm from this article:
+  // https://en.wikipedia.org/wiki/Longest_common_substring_problem
+  var l = new Array(s1.length)
+  for (i = 0; i < s1.length; ++i) {
+    l[i] = new Array(s2.length)
+  }
+  var z = 0;
+  var ret = ""
+  for (i = 0; i < s1.length; ++i) {
+    for (j = 0; j < s2.length; ++j) {
+      if (s1[i] == s2[j]) {
+        if (i == 0 || j == 0) {
+          l[i][j] = 1;
+        } else {
+          l[i][j] = l[i - 1][j - 1] + 1
+        }
+        if (l[i][j] > z) {
+          z = l[i][j];
+          ret = s1.substr(i - z + 1, z)
+        } else {
+         /*
+          if (l[i][j] == z) {
+            //ret += s1.substr(i - z + 1, z)
+          }*/
+        }
+      } else {
+        l[i][j] = 0;
+      }
+    }
+  }
+  return ret;
+}
+
+function reduce_lines(str_array) {
+  var s1 = str_array[0]
+  var s2 = str_array[str_array.length - 1]
+  var com_strings = []
+  var common_string = find_longest_substr(s1, s2)
+  var ind = 0;
+  while (common_string.length >= 4) {
+    s1 = s1.replace(common_string, "")
+    s2 = s2.replace(common_string, "")
+    com_strings.push(common_string)
+    common_string = find_longest_substr(s1, s2)
+    if (++ind > 3) {
+      break;
+    }
+  }
+  for (i = 0; i < str_array.length; ++i) {
+    for (j = 0; j < com_strings.length; ++j) {
+      if (str_array[i].indexOf(com_strings[j]) == -1) {
+        com_strings.splice(j, 1)
+        break;
+      }
+    }
+  }
+  for (i = 0; i < str_array.length; ++i) {
+    for (j = 0; j < com_strings.length; ++j) {
+      str_array[i] = str_array[i].replace(com_strings[j], "")
+    }
+  }
+  return str_array;
+}
+
+var path = []
+var last_position = 0
+
+function browseHandle(data){
+   var list = JSON.parse(data)
+   $('#lst-dir').empty()
+   var titles = []
+
+   $('#browser-hdr').text('/'+path.join('/'))
+   if(path.length != 0){
+      $('#lst-dir').append('<li>..</li>')
+   }
+   var dirs = list["dirs"]
+   var files = list["files"]
+   var file_display = files
+   if (path.length != 0 ){
+      file_display = reduce_lines(files.slice(0))
+   }
+   title_to_info = {}
+   for (dir in dirs ){
+      var title = dirs[dir]
+      var el = $('<li>', { class:'dirItem'}).html(title)
+      $('#lst-dir').append(el)
+      }
+   for (file in files ){
+      var title = file_display[file]
+      var file_name = files[file]
+      $('#lst-dir').append(format('<li filename="%s" class="fileItem">%s</li>', [file_name, title]))
+      }
+   $('#lst-dir').listview('refresh')
+   $.mobile.loading("hide")
+   $(window).scrollTop(last_position)
+}
+
+function itemLoaded(data){
+   $.mobile.loading("hide")
+}
+
+$('#lst-dir').on('click', 'li', function(){
+   var item = $(this).html()
+   var isDir = $(this).is(".dirItem")
+   $.mobile.loading("show",{ 
+      text:item,
+      textVisible:true 
+      })
+   $('input[data-type="search"]').val("");
+   if (item == '..'){
+      path.pop()
+      $('#lst-dir').empty().listview("refresh")
+      $.get('srv/browse/'+ path.join('/'), browseHandle)
+      return  
+   }
+   last_position = $(window).scrollTop()
+   if (!isDir){
+      var filename = this.getAttribute('filename')
+      $.get('srv/browse/'+path.join('/')+'/'+filename , itemLoaded)
+   } else {
+      var id = title_to_info[item]
+      path.push(item)
+      $('#lst-dir').empty().listview("refresh")
+      $.get('srv/browse/'+path.join('/'), browseHandle)
+   }
+})
+
+$("#btn-browse").bind("click", function(event, ui){
+   window.open('#browser', '_self')
+   $.mobile.loading("show",{ 
+      text:'/',
+      textVisible:true 
+      })
+   $.get('srv/browse/'+path.join('/'), browseHandle)
+})
+//-----------------Radio station-------------------------//
+var current_page = 1
+function stationsLoaded(data)
+{
+   try{
+       var stations = JSON.parse(data)
+    }
+    catch(e){
+       window.alert(e.message + "\n Data:\n" + data)
+       return
+    }
+   $('#lst-stations').empty()
+   $('#stations-hdr').text('Page ' + current_page)
+   for (station in stations)
+   {
+      var title = stations[station]['name']
+      var st_id = stations[station]['id']
+      $('#lst-stations').append('<li class="station" st_id="'+st_id+'">'
+                                 + title +
+                                '</li>')
+   }
+   $('#lst-stations').listview('refresh')
+}
+$('#lst-stations').on('click', 'li', function(){
+   var item = $(this).html()
+    var st_id = this.getAttribute('st_id')
+    $.get('srv/radio/play/' + st_id )
+
+})
+
+$('#btn-stations').bind('click', function(event, ui){
+   window.open('#stations', '_self')
+   $.get('srv/radio/page/'+current_page, stationsLoaded)
+
+})
+
+$('#btn-prev').bind('click', function(event, ui){
+    if(current_page > 1){
+        $.get('srv/radio/page/' + --current_page, stationsLoaded)
+    }
+})
+$('#btn-next').bind('click', function(event, ui){
+    $.get('srv/radio/page/' + ++current_page, stationsLoaded)
+})
+
+$('#btn-stop').bind('click', function(event, ui){
+    $.get('srv/radio/stop')
+})
+//-----------------Twitch  -------------------------//
 var twitch_page = 0
 var games_page = 0
 var current_game = ""
